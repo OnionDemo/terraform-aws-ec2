7ec89615e2bb7c33360a2df4871fc7b0a11911c8
Ting <ting@hashicorp.com>
2024-12-02 17:37:18 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,8 +30,8 @@ locals {
 # Create AWS Compute Instance 
 resource "aws_instance" "ec2_test" {
   ami           = data.aws_ami.amzn-linux-2023-ami.id
-  instance_type = "t2.micro"
-  //instance_type = "t2.nano"
+  //instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "EC2 ServiceNow Test ${local.simple_ec2_count +1}"
